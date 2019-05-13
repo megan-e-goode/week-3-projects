c3650be2f8ea0fea88dacbb06037129b4e7199c5
Megan Goode <megan.e.goode@hotmail.com>
2019-05-13 23:54:11 +0100

    Added test to return number between 1 and 6. Added code to pass this test.

--- ce-test-drive-a-dice-class/spec/game_spec.rb
+++ ce-test-drive-a-dice-class/spec/game_spec.rb
@@ -5,4 +5,9 @@ describe Dice do
     dice = Dice.new
     expect(dice).to respond_to(:roll)
   end
+
+  it '.roll returns random number between 1 and 6' do
+    dice = Dice.new
+    expect(dice.roll).to be_between(1, 6).inclusive
+  end
 end
