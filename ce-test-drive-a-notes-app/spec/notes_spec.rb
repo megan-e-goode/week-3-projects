f8e6b2a208e1016609f4c11254f27d50f7ee60b8
Megan Goode <megan.e.goode@hotmail.com>
2019-05-16 10:26:40 +0100

    Refectored creating new instance of Note in tests

--- ce-test-drive-a-notes-app/spec/notes_spec.rb
+++ ce-test-drive-a-notes-app/spec/notes_spec.rb
@@ -1,13 +1,13 @@
 require 'notes'
 
 describe Note do
+  let(:note) { Note.new }
+
   it 'creates new instance of Note' do
-    note = Note.new
     expect(note).to be_an_instance_of(Note)
   end
 
   it 'initializes note_list' do
-    note = Note.new
     expect(note.notes_list).to eql([])
   end
 
